a4d36d4205ea652d006c592ccbfce309f21a57e2
Juan Calderon <juanestebancredifacil@gmail.com>
2024-07-18 11:45:36 -0500

    chore: Update login form styling and remove unused files

--- dist/index.js
+++ dist/index.js
@@ -8,20 +8,42 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { BooksControllers } from "./classes/books-controllers.js";
+import { AllBooks } from "./classes/get.books.js";
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         const buttonLogin = document.querySelector('.submitLogin');
-        buttonLogin.addEventListener('click', (e) => __awaiter(this, void 0, void 0, function* () {
-            e.preventDefault();
-            const email = document.querySelector('.email').value;
-            const password = document.querySelector('.password').value;
-            const booksControllers = new BooksControllers('http://190.147.64.47:5155/', 'api/v1/auth/login');
-            let token = yield booksControllers.postLogin({ email: `${email}`, password: `${password}` });
-            localStorage.setItem('token', token);
-            if (localStorage.getItem('token')) {
-                window.location.href = 'pages/dashboard.html';
-            }
-        }));
+        if (buttonLogin) {
+            buttonLogin.addEventListener('click', (e) => __awaiter(this, void 0, void 0, function* () {
+                e.preventDefault();
+                const email = document.querySelector('.email').value;
+                const password = document.querySelector('.password').value;
+                const booksControllers = new BooksControllers('http://190.147.64.47:5155/', 'api/v1/auth/login');
+                let token = yield booksControllers.postLogin({ email: `${email}`, password: `${password}` });
+                localStorage.setItem('token', token);
+                if (localStorage.getItem('token')) {
+                    window.location.href = 'pages/dashboard.html';
+                }
+            }));
+        }
+        const bookCards = document.querySelector('.container-books');
+        console.log(bookCards);
+        const token = localStorage.getItem('token');
+        const allBooks = new AllBooks(`${token}`);
+        allBooks.getAllBooks().then((books) => {
+            books.forEach(book => {
+                const card = document.createElement('div');
+                card.className = 'card';
+                card.innerHTML = /*html*/ `
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
+                <p class="card-text">${book.description}</p>
+                <p class="card-text">${book.publicationDate}</p>
+            </div>
+            `;
+                bookCards === null || bookCards === void 0 ? void 0 : bookCards.appendChild(card);
+            });
+        });
     });
 }
 // {
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
 import { BooksControllers } from "./classes/books-controllers.js";
+import { AllBooks } from "./classes/get.books.js";
 async function main() {
 
-    const buttonLogin = document.querySelector('.submitLogin') as HTMLButtonElement
-    buttonLogin.addEventListener('click', async (e) => {
-        e.preventDefault()
-        const email = (document.querySelector('.email') as HTMLInputElement).value
-        const password = (document.querySelector('.password') as HTMLInputElement).value
+    const buttonLogin = document.querySelector('.submitLogin') as HTMLButtonElement;
+    if (buttonLogin) {
+        buttonLogin.addEventListener('click', async (e) => {
+            e.preventDefault();
+            const email = (document.querySelector('.email') as HTMLInputElement).value;
+            const password = (document.querySelector('.password') as HTMLInputElement).value;
 
-        const booksControllers = new BooksControllers('http://190.147.64.47:5155/', 'api/v1/auth/login');
-        let token: string = await booksControllers.postLogin({ email: `${email}`, password: `${password}` });
-        
-        localStorage.setItem('token', token);
+            const booksControllers = new BooksControllers('http://190.147.64.47:5155/', 'api/v1/auth/login');
+            let token: string = await booksControllers.postLogin({ email: `${email}`, password: `${password}` });
 
-        if (localStorage.getItem('token')) {
-            window.location.href = 'pages/dashboard.html';
-        }
+            localStorage.setItem('token', token);
+
+            if (localStorage.getItem('token')) {
+                window.location.href = 'pages/dashboard.html';
+            }
+        });
+    }
+
+    const bookCards = document.querySelector('.container-books') as HTMLDivElement;
+    console.log(bookCards);
+
+    const token = localStorage.getItem('token');
+
+    const allBooks = new AllBooks(`${token}`);
+    allBooks.getAllBooks().then((books) => {
+        books.forEach(book => {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.innerHTML = /*html*/`
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
+                <p class="card-text">${book.description}</p>
+                <p class="card-text">${book.publicationDate}</p>
+            </div>
+            `;
+            bookCards?.appendChild(card);
+        });
     });
 
-}
 
+}
 // {
 //     "email": "prueba@prueba.pru",
 //     "password": "C0ntr4S3gu++r4"
